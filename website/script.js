9833e25735cbb06f75f6bd76357d599269358d9f
Anne Norris <annemnorris09@gmail.com>
2025-04-10 09:44:53 -0400

    added about blurb

--- website/script.js
+++ website/script.js
@@ -1,11 +1,38 @@
-function joinClub(clubName) {
-    const userClubs = document.getElementById('user-clubs');
-    const existingClub = [...userClubs.children].some(li => li.textContent === clubName);
-    if (!existingClub) {
-      const li = document.createElement('li');
-      li.textContent = clubName;
-      userClubs.appendChild(li);
+const editBtn = document.getElementById("editBtn");
+let isEditing = false;
+
+editBtn.addEventListener("click", () => {
+  isEditing = !isEditing;
+  toggleEditMode(isEditing);
+});
+
+function toggleEditMode(editing) {
+  const inputs = ["name", "email", "address"];
+  inputs.forEach((field) => {
+    const display = document.getElementById(`${field}Display`);
+    const input = document.getElementById(`${field}Input`);
+    if (editing) {
+      display.classList.add("hidden");
+      input.classList.remove("hidden");
     } else {
-      alert('You are already in this book club!');
+      display.textContent = input.value.replace(/\n/g, "\n");
+      display.classList.remove("hidden");
+      input.classList.add("hidden");
     }
+  });
+
+  editBtn.innerHTML = editing ? "Save <span class='edit-icon'>💾</span>" : "Edit <span class='edit-icon'>✏️</span>";
+}
+
+function joinClub(clubName) {
+  const userClubs = document.getElementById('user-clubs');
+  const existingClub = [...userClubs.children].some(li => li.textContent === clubName);
+  if (!existingClub) {
+    const li = document.createElement('li');
+    li.textContent = clubName;
+    userClubs.appendChild(li);
+  } else {
+    alert('You are already in this book club!');
   }
+}
+
